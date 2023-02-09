95710358355d26cfd944cf96f5a10ce62fe81e12
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-02-09 13:00:46 -0300

    add postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-git pull
-python manage.py makemigrations guia
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+git pull &&
+python manage.py makemigrations guia &&
+python manage.py migrate &&
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell &&
+
 
 
 gunicorn guiasuldabahia.wsgi:application --bind 0.0.0.0:8000
--- guiasuldabahia/settings.py
+++ guiasuldabahia/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'guiasuldabahia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
