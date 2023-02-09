a8277b059b0734d2d960ce7dd2dd1db1905273af
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-02-09 13:44:09 -0300

    adiciona postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-git pull
-python manage.py makemigrations guia
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+git pull &&
+python manage.py makemigrations guia &&
+python manage.py migrate &&
+python manage.py collectstatic &&
+
 
 
 gunicorn guiasuldabahia.wsgi:application --bind 0.0.0.0:8000
