bb4a3d9957e6e6623189ea5da506bcea1822477e
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-02-09 20:27:32 -0300

    add postgres

--- guiasuldabahia/settings.py
+++ guiasuldabahia/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'guiasuldabahia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nome_do_seu_banco_de_dados',
+        'USER': 'nome_do_seu_usuario',
+        'PASSWORD': 'senha_do_seu_usuario',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
