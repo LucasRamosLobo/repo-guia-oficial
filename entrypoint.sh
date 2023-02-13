a5ceed30b50527cff39071524fea7932a33d7597
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-02-13 16:58:46 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
-git pull 
-python manage.py makemigrations guia 
-python manage.py migrate 
+git pull origin master
+python manage.py makemigrations guia &&
+python manage.py migrate &&
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell &&
+
+
+
 gunicorn guiasuldabahia.wsgi:application --bind 0.0.0.0:8000
