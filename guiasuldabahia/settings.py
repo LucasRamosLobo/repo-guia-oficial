6cf0a95ae67ad4ff6ca1e1d3870f332a68740e97
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-02-15 23:13:49 -0300

    add mysql

--- guiasuldabahia/settings.py
+++ guiasuldabahia/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'guiasuldabahia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dados',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
